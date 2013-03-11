2a18bed78c796457816ae70abbd974eaf0954a96
Tommyko <tommyko2006@gmail.com>
2013-03-11 14:42:32 +0800

    hahahahah e for your changes. Lines starting

---
+++ FYP/src/edu/fyp/bean/User.java
@@ -0,0 +1,29 @@
+package edu.fyp.bean;
+
+class User{
+protected String userName;
+protected String password;
+protected String empid;         
+
+public void setUserName(String userName){
+this.userName = userName;}
+
+public String getUserName(){
+return userName;
+}
+
+public void setPassword(String password){
+this.password = password;}
+
+public String getPassword(){
+return password;
+}
+
+public void setEmpid(String empid){
+this.empid = empid;}
+
+public String getEmpid(){
+return empid;
+}
+
+}
