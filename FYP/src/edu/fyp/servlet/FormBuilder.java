8f7a27e157050d45dabdf9878f2ca895e9d66c5a
unknown <english@lrclibnb59.vtc.hk>
2013-03-13 14:36:01 +0800

    add testing servlet

---
+++ FYP/src/edu/fyp/servlet/FormBuilder.java
@@ -0,0 +1,37 @@
+package edu.fyp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FormBuilder extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<form>This is form.</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		processRequest(req, resp);
+	}
+
+	protected void processRequest(HttpServletRequest req,
+			HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println(req.getParameter("formHtml"));
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+}
