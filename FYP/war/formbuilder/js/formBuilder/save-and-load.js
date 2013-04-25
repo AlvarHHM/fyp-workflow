0313332ea21b38d3c65d4f542737957f6f70e175
Ken <kencplee@gmail.com>
2013-04-25 20:52:24 +0800

    Fixing Save and Load
    Truly worked with correct post path

--- FYP/war/formbuilder/js/formBuilder/save-and-load.js
+++ FYP/war/formbuilder/js/formBuilder/save-and-load.js
@@ -1,7 +1,15 @@
-//
-
-var form = "";
 $(document).ready(function(){
+	//Load form at start
+	overlaying();
+	
+	$(".form-item").draggable({
+		containment: "parent",
+		stop: function(event, ui){
+			selectFormItem($(ui.draggable).attr("id"));
+		}
+		}).click(function(){
+			selectFormItem($(this).attr("id"));
+		});
 	$("#form-save").click(function(){
 		
 		//deselect the item
@@ -10,67 +18,31 @@ $(document).ready(function(){
 		$("#formBuilder-leftPanel ul li:nth-child(2)").hide();
 		$("#formBuilder-leftPanel").tabs("option", "active", 0);
 		
-		//remove overlay and save the form
+		//remove overlay
 		$(".form-item .overlay").remove();
-		form = $('#formBuilder-stage').html();
 		
-		//add overlay to new item
-		$(".form-item").each(function(i,e){
-			if($(e).children(".overlay").length == 0)
-				$(e).append("<div class='overlay' ></div>");
-		});
+		//save the form
+		var form = $('#formBuilder-stage').html();
+		
+		//add overlay back
+		overlaying();
 		
 		//sending request
-		var params = {
-					"FormHtml" : form , 
-					"Title" : $("#form-title").html(),
-					"Constraint" : ""};
-		post_to_url(
-			"http://localhost:8888/formBuilder",
-			//"http://www.hashemian.com/tools/form-post-tester.php",
-			params, "post");
+		$.post("http://localhost:8888/formBuilder"
+			,{
+				FormHtml : form, 
+				Title : $("#form-title").html(),
+				Constraint : ""}
+			, function(data) {
+				alert(data);
+			});
 	})
-	$("#form-load").click(function(){
-		
-		//add overlay to new item
-		$(".form-item").each(function(i,e){
-			if($(e).children(".overlay").length == 0)
-				$(e).append("<div class='overlay' ></div>");
-		});
-	});
 });
 
-function post_to_url(path, params, method) {
-    method = method || "post"; 
-
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-         }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
+//add overlay to new item
+function overlaying(){
+	$(".form-item").each(function(i,e){
+		if($(e).children(".overlay").length == 0)
+			$(e).append("<div class='overlay' ></div>");
+	});
 }
-//Code for reading a saved form
-    /*
-     $(".form-item").append(
-     "<div class='overlay' ></div>");
-     $(".form-item").draggable({
-     containment: "parent",
-     stop: function(event, ui){
-     selectFormItem($(ui.draggable).attr("id"));
-     }
-     }).click(function(){
-     selectFormItem($(this).attr("id"));
-     });
-     */
