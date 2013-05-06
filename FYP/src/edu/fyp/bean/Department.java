9ee98f8358ce7443b8bee666bdd9ebab2fb5610f
mahoihei <mahoihei@gmail.com>
2013-05-06 11:51:38 +0800

    annotate department

--- FYP/src/edu/fyp/bean/Department.java
+++ FYP/src/edu/fyp/bean/Department.java
@@ -1,15 +1,27 @@
 package edu.fyp.bean;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
 class Department {
-	protected String deptId;
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	protected Key deptId;
+	@Persistent
 	protected String deptName;
 
-	public void setDeptId(String deptId) {
-		this.deptId = deptId;
+	
+	public Key getDeptId() {
+		return deptId;
 	}
 
-	public String getDeptId() {
-		return deptId;
+	public void setDeptId(Key deptId) {
+		this.deptId = deptId;
 	}
 
 	public void setDeptName(String deptName) {
