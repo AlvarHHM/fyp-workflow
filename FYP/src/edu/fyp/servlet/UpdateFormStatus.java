bddba3653c9d0b86d262386568eef85db3d44b91
unknown <autscr@gmail.com>
2013-05-13 13:50:46 +0800

    deploy version

--- FYP/src/edu/fyp/servlet/UpdateFormStatus.java
+++ FYP/src/edu/fyp/servlet/UpdateFormStatus.java
@@ -2,11 +2,6 @@ package edu.fyp.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Enumeration;
-import java.util.HashMap;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,19 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import com.google.appengine.api.datastore.Text;
-import com.google.appengine.labs.repackaged.org.json.JSONArray;
-import com.google.appengine.labs.repackaged.org.json.JSONException;
-import com.google.appengine.labs.repackaged.org.json.JSONObject;
-import com.google.gson.Gson;
-
-import edu.fyp.bean.node.ApproveNode;
-import edu.fyp.bean.node.NoticeNode;
-import edu.fyp.bean.node.PathNode;
-import edu.fyp.bean.node.StartNode;
-import edu.fyp.factory.PathNodeFactory;
 import edu.fyp.manager.FormManager;
-import edu.fyp.repository.PathNodeRepository;
 
 public class UpdateFormStatus extends HttpServlet {
 	
@@ -48,7 +31,6 @@ public class UpdateFormStatus extends HttpServlet {
 		String status = req.getParameter("status");
 		if (formID.equalsIgnoreCase("") || version.equalsIgnoreCase("")
 				|| status.equalsIgnoreCase("")) {
-			//out.println("Form ID, Version, Path can not be empty.");
 			return ;
 		}
 		try {
